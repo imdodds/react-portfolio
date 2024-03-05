dc9ab9476ccf606fc1e3290e539dfb2753f714b1
Ian Dodds <ianmitchelldodds@gmail.com>
2024-03-05 17:32:38 -0500

    Add Portfolio and PortfolioItem components

---
+++ src/data/portfolio.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    title: 'Portfolio Item 1',
+    imgUrl: 'assets/img.png',
+    stack: ['React', 'Tailwind', 'Node', 'Express', 'MongoDB'],
+    description: 'Description goes here...',
+    link: 'https://github.com'
+  },
+  {
+    title: 'Portfolio Item 2',
+    imgUrl: 'assets/img.png',
+    stack: ['React', 'Tailwind', 'Node', 'Express', 'MongoDB'],
+    description: 'Description goes here...',
+    link: 'https://github.com'
+  },
+  {
+    title: 'Portfolio Item 3',
+    imgUrl: 'assets/img.png',
+    stack: ['React', 'Tailwind', 'Node', 'Express', 'MongoDB'],
+    description: 'Description goes here...',
+    link: 'https://github.com'
+  },
+]
