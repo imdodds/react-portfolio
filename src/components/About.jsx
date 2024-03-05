d8ad26e8bd3efb924d1452e285a8d3271ab56115
Ian Dodds <ianmitchelldodds@gmail.com>
2024-03-05 16:23:31 -0500

    Add About component

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
+      <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>
+        Ian Mitchell Dodds
+      </h1>
+      <p className='text-base md:text-xl mb-3 font-medium'>
+        Full Stack Developer
+      </p>
+      <p className='text-sm max-w-xl mb-6'>
+        Bio goes here...
+      </p>
+    </div>
+  )
+}
+
+export default About
