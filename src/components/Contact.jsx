34499774aa2e18069450b3c23d4bf989962cdce0
Ian Dodds <ianmitchelldodds@gmail.com>
2024-03-06 10:08:22 -0500

    Add Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      Contact
+    <div className='flex flex-col mb-10 mx-auto'>
+      <div className='flex justify-center items-center'>
+        <form
+          action="https://getform.io/f/nbvkgvva"
+          method="POST"
+          className='flex flex-col w-full md:w-7/12'
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            className='p-2 bg-transparent border-2 rounded-md focus:outline-none'
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            className='my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none'
+          />
+          <textarea
+            name="message"
+            placeholder="Message"
+            rows="10"
+            className='p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none'
+          />
+          <button
+            type="button"
+            className=' inline-block text-center px-8 py-3 w-max text-base font-medium rounded-md text-white bg-stone-700 drop-shadow-md'
+          >
+            Get in touch
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
