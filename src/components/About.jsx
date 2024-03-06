7c38395e14e6cec26cbb2997eeaa71f77a44a274
Ian Dodds <ianmitchelldodds@gmail.com>
2024-03-06 13:25:44 -0500

    Add Social Links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFileAlt } from "react-icons/fa";
+
+
 const About = () => {
   return (
     <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
@@ -12,6 +17,27 @@ const About = () => {
       <p className='text-sm max-w-xl mb-6'>
         Bio goes here...
       </p>
+      <div className='flex flex-row p-2 text-center'>
+
+        <a className='hover:text-stone-400' href='https://github.com/imdodds'>
+          <FaGithub className='inline-block mx-1 mb-2' size={24} />
+          <p className='text-xs mx-2'>
+            Github
+          </p>
+        </a>
+        <a className='hover:text-stone-400' href='https://www.linkedin.com/in/ian-dodds/'>
+          <FaLinkedin className='inline-block mx-1 mb-2' size={24} />
+          <p className='text-xs mx-2'>
+            LinkedIn
+          </p>
+        </a>
+        <a className='hover:text-stone-400' href='https://resume.creddle.io/resume/j49g9z35g33'>
+          <FaFileAlt className='inline-block mx-1 mb-2' size={24} />
+          <p className='text-xs mx-2'>
+            Resume
+          </p>
+        </a>
+      </div>
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <div className='p-5 text-center'>
-      { /* Social Icons */}
+
+      <a href='https://github.com/imdodds'>
+        <FaGithub className='inline-block mx-3 mb-4 hover:text-stone-400' size={36} />
+      </a>
+      <a href='https://www.linkedin.com/in/ian-dodds/'>
+        <FaLinkedin className='inline-block mx-3 mb-4 hover:text-stone-400' size={36} />
+      </a>
       <p className='text-sm mt-2 opacity-50'>
         &copy; {new Date().getFullYear()} Ian Mitchell Dodds. All rights reserved.
       </p>
